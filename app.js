11480d7b3f7a3acaef799bd8d7b6a2509f899f08
Cho <brandon.cho@kronos.com>
2017-10-04 17:31:59 -0400

    set up routes and ng-view

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // module
-var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource'])
+						.config(function ($routeProvider){
+
+							$routeProvider
+
+							.when('/', {
+								templateUrl: './pages/home.html',
+								controller: 'homeController'
+							})
+
+							.when('/forecast', {
+								templateUrl: './pages/forecast.html',
+								controller: 'forecastController'
+							})
+
+						})
+						.controller('homeController', function($scope){
+
+						})
+						.controller('forecastController', function($scope){
+
+						});
