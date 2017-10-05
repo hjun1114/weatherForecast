5e0135e5a416dec54dce0be53733e86833417808
Cho <brandon.cho@kronos.com>
2017-10-05 17:05:54 -0400

    api calling with resource service - confusing

--- app.js
+++ app.js
@@ -28,8 +28,20 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource'])
 							});
 						})
 
-						.controller('forecastController', function($scope, cityService){
+						.controller('forecastController', function($scope, $resource, cityService){
+							// resource services makes it easier for us to go out and get data.
+							// basically wraps up the http service 
 							$scope.city = cityService.city;
+
+							$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=4e57b5e64064ea5cab3cb2b7e56449d0", 
+							{
+								callback: "JSON_CALLBACK"
+							},
+							{
+								get: {method: "JSONP"}
+							})
+							$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt:2 });
+							console.log($scope.weatherResult);
 						});
 
 // http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=4e57b5e64064ea5cab3cb2b7e56449d0
